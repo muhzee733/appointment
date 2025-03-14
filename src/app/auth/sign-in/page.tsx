2e2d4d0889fec6fce64d9de2dddca2214989e869
basit ali <cool.saa733@gmail.com>
2025-03-14 14:33:17 +0400

    added

--- src/app/auth/sign-in/page.tsx
+++ src/app/auth/sign-in/page.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import type { Metadata } from 'next';
 
 import { config } from '@/config';
-// import { GuestGuard } from '@/components/auth/guest-guard';
 import { Layout } from '@/components/auth/layout';
 import { SignInForm } from '@/components/auth/sign-in-form';
 
